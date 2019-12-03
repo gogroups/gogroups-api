2e8105ca48c4a14e03c4b731a96a9c0a6b1becc4
Mitin Dixit <midixi@microsoft.com>
2019-12-04 01:20:25 +0530

    updated smtp

--- Web+API/application/config/email.php
+++ Web+API/application/config/email.php
@@ -1,10 +1,10 @@
 <?php
 
  		 $config['protocol']= 'smtp';  
-         $config['smtp_host']    = 'ssl://smtp.zoho.com'; 
+         $config['smtp_host']    = 'ssl://smtp.zoho.in'; 
          $config['smtp_port']    = '465'; 
-         $config['smtp_user']    = 'pawan.gulati@zabius.com';
-         $config['smtp_pass']    = 'zabius@1234'; 
+         $config['smtp_user']    = 'virframe@zabius.com';
+         $config['smtp_pass']    = 'avatarang@123'; 
          $config['newline']    = "\r\n";  
          $config['mailtype'] = 'text'; // or html    
 
