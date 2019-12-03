a363bafc339e94d2a1d4d754ab5a74eee5340f46
Mitin Dixit <midixi@microsoft.com>
2019-12-04 00:34:29 +0530

    changed smtp address

--- Web+API/application/config/email.php
+++ Web+API/application/config/email.php
@@ -1,7 +1,7 @@
 <?php
 
  		 $config['protocol']= 'smtp';  
-         $config['smtp_host']    = 'ssl://smtp.zoho.com'; 
+         $config['smtp_host']    = 'ssl://smtp.zoho.in'; 
          $config['smtp_port']    = '465'; 
          $config['smtp_user']    = 'pawan.gulati@zabius.com';
          $config['smtp_pass']    = 'zabius@1234'; 
