1a6c4b0067dc8432d8488af8bee4966dbb28178e
Mitin Dixit <midixi@microsoft.com>
2019-12-04 01:07:05 +0530

    reverting the changes

--- Web+API/application/config/email.php
+++ Web+API/application/config/email.php
@@ -1,11 +1,10 @@
 <?php
 
  		 $config['protocol']= 'smtp';  
-         $config['smtp_host']    = 'ssl://smtp.gmail.com';
+         $config['smtp_host']    = 'ssl://smtp.zoho.com'; 
          $config['smtp_port']    = '465'; 
-         $config['smtp_user']    = 'virframe@gmail.com';
-         $config['smtp_pass']    = 'Avatarang@10132';
-         $config['auth']    =  true;
+         $config['smtp_user']    = 'pawan.gulati@zabius.com';
+         $config['smtp_pass']    = 'zabius@1234'; 
          $config['newline']    = "\r\n";  
          $config['mailtype'] = 'text'; // or html    
 
