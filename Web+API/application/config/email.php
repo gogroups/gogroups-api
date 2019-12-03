e95a72687281c9a6460d7c5cdc56314024a7774c
Mitin Dixit <midixi@microsoft.com>
2019-12-04 00:54:22 +0530

    changes smtp server to google

--- Web+API/application/config/email.php
+++ Web+API/application/config/email.php
@@ -1,10 +1,11 @@
 <?php
 
  		 $config['protocol']= 'smtp';  
-         $config['smtp_host']    = 'ssl://smtp.zoho.in'; 
+         $config['smtp_host']    = 'ssl://smtp.gmail.com';
          $config['smtp_port']    = '465'; 
-         $config['smtp_user']    = 'pawan.gulati@zabius.com';
-         $config['smtp_pass']    = 'zabius@1234'; 
+         $config['smtp_user']    = 'virframe@gmail.com';
+         $config['smtp_pass']    = 'Avatarang@10132';
+         $config['auth']    =  true;
          $config['newline']    = "\r\n";  
          $config['mailtype'] = 'text'; // or html    
 
