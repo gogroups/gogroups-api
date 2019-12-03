05cfd715be3ae84bac2efb9f921ce5447d626032
Mitin Dixit <midixi@microsoft.com>
2019-12-04 01:23:16 +0530

    changed mail

--- Web+API/application/config/email.php
+++ Web+API/application/config/email.php
@@ -3,7 +3,7 @@
  		 $config['protocol']= 'smtp';  
          $config['smtp_host']    = 'ssl://smtp.zoho.in'; 
          $config['smtp_port']    = '465'; 
-         $config['smtp_user']    = 'virframe@zabius.com';
+         $config['smtp_user']    = 'virframe@zohomail.in';
          $config['smtp_pass']    = 'avatarang@123'; 
          $config['newline']    = "\r\n";  
          $config['mailtype'] = 'text'; // or html    
